956151def1b51c674b5fa717d5fcc5bb27b5575d
Josh Ghimire <ghimirea@umich.edu>
2024-01-30 13:17:30 -0500

    Final corrections

--- HW2/Code/Assignment2GhimireJosh.m
+++ HW2/Code/Assignment2GhimireJosh.m
@@ -5,16 +5,15 @@ vector2 = 1:1000;
 vector3 = ceil(rand(1,100)*10);
 
 % find indices where vector is b/t 5 and 10 (inclusive) or greater than 20
-v1Indices = find(vector1>=5 & vector1<=10 | vector1 >20);   
-v2Indices = find(vector2>=5 & vector2<=10 | vector2 >20);   
-v3Indices = find(vector3>=5 & vector3<=10 | vector3 >20);   
+v1Indices = find(vector1>=5 & vector1<=10 | vector1 >20)   
+v2Indices = find(vector2>=5 & vector2<=10 | vector2 >20);   % silenced due to length of result
+v3Indices = find(vector3>=5 & vector3<=10 | vector3 >20);   % silenced
 %% Q2. 
 x = 10:20;
 y = 10*(ones(1,11));
 figure
 plot(x,y)
-xlim([0, 30]);
-ylim([0,30]);
+axis([0 30 0 30])
 hold on
 
 x2 = 10:20;
@@ -28,7 +27,7 @@ plot(x3, y3);
 x4 = 20*(ones(1,11));
 y4 = 10:20;
 plot(x4,y4);
-title('Behold the Square')
+title('Behold, the Square!')
 
 %% Q3.
 lengthOfVector = 100000;
@@ -58,7 +57,8 @@ histogram(V2, binCenters)
 xlabel('Overwatch Rank')
 ylabel('Cortisol Level')
 title('Stress Levels vs. Overwatch Rank')
-
+legend('Goons', 'Homies')
+% This makes more sense if you don't think about it.
 %% Q5. 
 figure
 plotMyLine(1,2,'red')
